07a04d704e14bad86dd86cb80291b958f2c09f2d
Irwin <irwin.soliman@gmail.com>
2025-03-14 17:45:51 +0100

    :construction: WIP workflow not working with front interceptors/token. SecurityConfig to adapt and route updateFollow for association to test

--- src/main/java/com/back_alasso/AssociationFollower/AssociationFollowerRepository.java
+++ src/main/java/com/back_alasso/AssociationFollower/AssociationFollowerRepository.java
@@ -1,6 +1,9 @@
 package com.back_alasso.AssociationFollower;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssociationFollowerRepository extends JpaRepository<AssociationFollower, UUID> {}
+public interface AssociationFollowerRepository extends JpaRepository<AssociationFollower, UUID> {
+  Optional<AssociationFollower> findByVoluntary_idAndAssociation_id(UUID authenticatedUser, UUID associationId);
+}
