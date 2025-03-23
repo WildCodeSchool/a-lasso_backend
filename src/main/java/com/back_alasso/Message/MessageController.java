0b4058fb917bff896a7feeea14cf991042ab14bf
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-03-23 14:08:33 +0100

    fix cors problem for authenticated routes in securityConfig + how get userAuthenticated in backend

--- src/main/java/com/back_alasso/Message/MessageController.java
+++ src/main/java/com/back_alasso/Message/MessageController.java
@@ -1,35 +1,43 @@
 package com.back_alasso.Message;
 
 import com.back_alasso.Authentication.AuthService;
+import com.back_alasso.User.UserService;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/messages")
-@CrossOrigin(origins = "http://localhost:4200") // Allow Angular frontend
 public class MessageController {
 
   public final MessageService messageService;
   public final AuthService authService;
+  public final UserService userService;
 
-  public MessageController(MessageService messageService, AuthService authService) {
+  public MessageController(MessageService messageService, AuthService authService, UserService userService) {
     this.messageService = messageService;
     this.authService = authService;
+    this.userService = userService;
   }
 
   @GetMapping("/{activityId}")
-  public ResponseEntity<List<MessageDTO>> getAllActivityMessages(@PathVariable UUID activityId, @RequestHeader("Authorization") String token) {
-    UUID authenticatedUser = authService.getUserIdFromToken(token);
+  public ResponseEntity<List<MessageDTO>> getAllActivityMessages(@PathVariable UUID activityId, @AuthenticationPrincipal UserDetails userDetails) {
+    String emailAuthenticatedUser = userDetails.getUsername();
+    UUID authenticatedUser = userService.findByEmail(emailAuthenticatedUser).getId();
+
     List<MessageDTO> messages = messageService.getAllActivityMessages(activityId, authenticatedUser);
     return ResponseEntity.status(HttpStatus.OK).body(messages);
   }
 
   @PostMapping
-  public ResponseEntity<MessageDTO> createMessage(@RequestBody MessageCreationDTO newMessage, @RequestHeader("Authorization") String token) {
-    UUID authenticatedUser = authService.getUserIdFromToken(token);
+  public ResponseEntity<MessageDTO> createMessage(@RequestBody MessageCreationDTO newMessage, @AuthenticationPrincipal UserDetails userDetails) {
+    String emailAuthenticatedUser = userDetails.getUsername();
+    UUID authenticatedUser = userService.findByEmail(emailAuthenticatedUser).getId();
+
     MessageDTO savedMessage = messageService.createMessage(newMessage, authenticatedUser);
     return ResponseEntity.status(HttpStatus.CREATED).body(savedMessage);
   }
