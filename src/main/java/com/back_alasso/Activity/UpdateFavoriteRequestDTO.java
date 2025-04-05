ee0b3df6de4a74cd1e3b247dc141a0affdb972a9
Irwin <irwin.soliman@gmail.com>
2025-04-05 19:37:28 +0200

    :hammer: favorite activity button is now persited in dataBase

---
+++ src/main/java/com/back_alasso/Activity/UpdateFavoriteRequestDTO.java
@@ -0,0 +1,3 @@
+package com.back_alasso.Activity;
+
+public record UpdateFavoriteRequestDTO(Boolean isFavorite) {}
--- src/main/java/com/back_alasso/security/SecurityConstants.java
+++ src/main/java/com/back_alasso/security/SecurityConstants.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class SecurityConstants {
 
-  public static final Set<String> PUBLIC_URLS = new HashSet<>(Arrays.asList("/auth/**", "/activities", "/association/*", "/images/**"));
+  public static final Set<String> PUBLIC_URLS = new HashSet<>(
+    Arrays.asList("/auth/**", "/activities", "/activities/{id}", "/association/{id}", "/images/**")
+  );
 
-  public static final Set<String> PRIVATE_URLS = new HashSet<>(Arrays.asList("/association/*/updateFollow"));
+  public static final Set<String> PRIVATE_URLS = new HashSet<>(Arrays.asList("/association/*/updateFollow", "/activities/*/updateFavorite"));
 }
