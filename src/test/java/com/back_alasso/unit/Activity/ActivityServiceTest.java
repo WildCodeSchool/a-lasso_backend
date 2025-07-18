0b1ac5f2940728db3110bec0466768a017106976
Irwin SOLIMAN <122315710+Irwin-SOLIMAN@users.noreply.github.com>
2025-07-18 11:21:14 +0200

    :sparkles: added the cd yml conf to deploy on VPS (#47)

--- src/test/java/com/back_alasso/unit/Activity/ActivityServiceTest.java
+++ src/test/java/com/back_alasso/unit/Activity/ActivityServiceTest.java
@@ -1,5 +1,7 @@
 package com.back_alasso.unit.Activity;
 
+
+import com.back_alasso.exception.ResourceNotFoundException;
 import com.back_alasso.features.Activity.ActivityResponseMapper;
 import com.back_alasso.features.ActivityImage.ActivityImageService;
 import com.back_alasso.features.ActivityTheme.ActivityThemeService;
@@ -12,7 +14,6 @@ import com.back_alasso.features.Address.Address;
 import com.back_alasso.features.Association.AssociationService;
 import com.back_alasso.features.Country.Country;
 
-import com.back_alasso.exception.ResourceNotFoundException;
 import com.back_alasso.features.Geolocation.GeolocationService;
 import com.back_alasso.features.Image.ImageService;
 import com.back_alasso.features.Theme.ThemeService;
