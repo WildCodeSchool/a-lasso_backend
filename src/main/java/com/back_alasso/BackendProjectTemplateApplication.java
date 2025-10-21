113a576577b5a09fda93b24e7cc270da8ad31302
Irwin <irwin.soliman@gmail.com>
2025-10-21 09:11:27 +0200

    fix after users review

--- src/main/java/com/back_alasso/BackendProjectTemplateApplication.java
+++ src/main/java/com/back_alasso/BackendProjectTemplateApplication.java
@@ -1,8 +1,10 @@
 package com.back_alasso;
 
 import com.back_alasso.config.InitLogger;
+import com.fasterxml.jackson.core.StreamReadConstraints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +12,10 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class BackendProjectTemplateApplication {
 
+  private final int ONE_HUNDRED_THOUSAND = 100_000_000;
+
   public static void main(String[] args) {
+    System.setProperty("spring.jackson.parser.allow-unquoted-field-names", "true");
     ApplicationContext context = SpringApplication.run(BackendProjectTemplateApplication.class, args);
     InitLogger logger = new InitLogger();
     logger.logCurrentEnvironment(context);
@@ -19,5 +24,13 @@ public class BackendProjectTemplateApplication {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  @Bean
+  public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+    return builder ->
+      builder.postConfigurer(mapper ->
+        mapper.getFactory().setStreamReadConstraints(StreamReadConstraints.builder().maxStringLength(ONE_HUNDRED_THOUSAND).build())
+      );
   }
 }
--- src/main/java/com/back_alasso/features/Authentication/AuthController.java
+++ src/main/java/com/back_alasso/features/Authentication/AuthController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.Validator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -39,6 +40,16 @@ public class AuthController {
   private final AssociationLoginResponseMapper associationLoginResponseMapper;
   private final LoginRateLimitFilter loginRateLimitFilter;
 
+  @GetMapping("/token")
+  public ResponseEntity<Map<String, Object>> getUserByToken(@AuthenticationPrincipal UserDetails userDetails) {
+    UUID authenticatedUserId = userService.getAuthenticatedUserId(userDetails);
+    User user = userService.findById(authenticatedUserId);
+
+    Map<String, Object> response = new HashMap<>();
+    authService.addUserToResponse(user, response);
+    return ResponseEntity.ok(response);
+  }
+
   @PostMapping("/register/voluntary")
   public ResponseEntity<Boolean> register(@Valid @RequestBody VoluntaryRegistrationDTO voluntaryRegistrationDTO) {
     userService.checkUserExists(voluntaryRegistrationDTO.email());
@@ -48,7 +59,7 @@ public class AuthController {
   }
 
   @PostMapping("/register/association")
-  public ResponseEntity<Boolean> register(@RequestBody AssociationRegistrationDTO associationRegistrationDTO) {
+  public ResponseEntity<Boolean> register(@Valid @RequestBody AssociationRegistrationDTO associationRegistrationDTO) {
     validator.validate(associationRegistrationDTO, OnPublish.class);
 
     userService.checkUserExists(associationRegistrationDTO.email());
